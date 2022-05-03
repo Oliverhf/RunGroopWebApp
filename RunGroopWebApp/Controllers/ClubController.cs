59dd09031092159977f0eb25cf498bd64e311748
unknown <oliver.hfavalli@gmail.com>
2022-05-03 02:34:10 -0300

    feat: Create Club

--- RunGroopWebApp/Controllers/ClubController.cs
+++ RunGroopWebApp/Controllers/ClubController.cs
@@ -25,6 +25,23 @@ namespace RunGroopWebApp.Controllers
             return View(club);
         }
 
+        public async Task<IActionResult> Create()
+        {
+
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(Club club)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(club);
+            }
+
+            _clubRepository.Add(club);
+            return RedirectToAction("Index");
+        }
 
 
     }
