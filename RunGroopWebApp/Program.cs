af6b3d3b04478bf36b3bdeeba1c3ff642f5c1c8a
unknown <oliver.hfavalli@gmail.com>
2022-05-05 23:33:09 -0300

    feat: Feat Identity framework setup and User/Roles seed

--- RunGroopWebApp/Program.cs
+++ RunGroopWebApp/Program.cs
@@ -4,6 +4,9 @@ using RunGroopWebApp.Interfaces;
 using RunGroopWebApp.Repository;
 using RunGroopWebApp.Helpers;
 using RunGroopWebApp.Services;
+using RunGroopWebApp.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Authentication.Cookies;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -17,13 +20,18 @@ builder.Services.AddDbContext<ApplicationDbContext>(options =>
 {
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
-
+builder.Services.AddIdentity<AppUser, IdentityRole>()
+       .AddEntityFrameworkStores<ApplicationDbContext>();
+builder.Services.AddMemoryCache();
+builder.Services.AddSession();
+builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+    .AddCookie();
 var app = builder.Build();
 
 if(args.Length == 1 && args[0].ToLower() == "seeddata") {
 
-    // Seed.SeedUsersAndRolesAsync(app);
-    Seed.SeedData(app);
+    await Seed.SeedUsersAndRolesAsync(app);
+    // Seed.SeedData(app);
 }
 
 // Configure the HTTP request pipeline.
