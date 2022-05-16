fec662818523fc683e3a4936c85e641930d43ff9
unknown <oliver.hfavalli@gmail.com>
2022-05-16 18:50:01 -0300

    feat: Edit Profile and IPO address

---
+++ RunGroopWebApp/Views/Dashboard/EditUserProfile.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace RunGroopWebApp.Views.Dashboard
+{
+    public class EditUserProfileModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
